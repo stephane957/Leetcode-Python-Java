d8d13470a66e3507db7d9c192d6d779ecda89053
Stephane Toyo Demanou <stephanetoyo14@gmail.com>
2025-09-22 18:14:20 -0400

    Changes to the Trapping Rain Water Python solution

--- python/TwoPointers/TrappingRainWater.py
+++ python/TwoPointers/TrappingRainWater.py
@@ -4,24 +4,26 @@ Difficulty: Hard
 Topics: Array, Two Pointers, Dynamic Programming, Stack, Monotonic Stack
 """
 def trap(height: list[int]) -> int:
-    if not height:
-        return 0
-    n = len(height)
-    left, left_max = 0, height[0]
-    right = n-1
-    right_max = height[n-1]
-    water = 0
+        if not height:
+            return 0
 
-    while left < right:
+        left, right = 0, len(height)-1
+        left_max, right_max = height[left], height[right]
+        total_water = 0
 
-        if left_max < right_max:
-            left += 1
-            left_max = max(left_max, height[left])
-            water += left_max - height[left]
+        while left < right:
+            # The water level is constrained by the smaller of the two maximums.
+            if left_max <= right_max:
+                # We shift the left pointer
+                left += 1
+            # Update left_max before calculating water to ensure it's the max to the left of the current bar.
+                left_max = max(left_max, height[left])
+                total_water += left_max - height[left]
+            else:
+                # We shift the right pointer
+                right -= 1
+            # Update right_max before calculating water to ensure it's the max to the right of the current bar.
+                right_max = max(right_max, height[right])
+                total_water += right_max - height[right]
 
-        else:
-            right -= 1
-            right_max = max(right_max, height[right])
-            water += right_max - height[right]
-            
-    return water
+        return total_water
